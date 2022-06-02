7ad8da4fca4d3f6546d95b03ef7d4c4a18ed75b9
DIOGO RIBEIRO GOMES <diogo.rgomes1@senacsp.edu.br>
2022-06-02 11:55:35 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, ScrollView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={estilo.container}>
+      <Text style= {estilo.titulo}>jogos aiaiaiia</Text>
+      <Text style= {estilo.jogo}>sluffy sola</Text>
       <StatusBar style="auto" />
+<ScrollView>
+      <Image style={estilo.img} source={require("./assets/mini.jpg")}/>
+      <Text style= {estilo.jogo2}>metal gear</Text>
+      <Image style={estilo.img} source={require("./assets/gear.jpg")}/>
+      </ScrollView>
     </View>
+   
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"green",
+   
   },
+
+  titulo:{
+    fontSize:40,
+    marginTop:20,
+    color: "red",
+   
+  },
+
+  img:{
+    width: 350,
+    height: 300,
+  },
+
+  jogo:{
+    marginTop:40,
+    fontSize:30,
+    marginBottom:34,
+  },
+
+  jogo2:{
+    marginTop:40,
+    fontSize:30,
+    marginBottom:34,
+    borderColor:"yellow",
+  }
+
 });
